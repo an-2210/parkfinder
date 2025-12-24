d66b2989e083004e0d493f2334e34539d4c0dbb8
imanchalsingh <singhanchal159357@gmail.com>
2025-12-24 14:31:14 +0530

    commit changes

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/parkingdb");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
